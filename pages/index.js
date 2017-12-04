492cf4d7fcf80ec2706bc1e9a24bbbd7201ae0c7
Andrew Yasinishyn <yasinishyn.a.n@gmail.com>
2017-12-04 23:42:23 +0200

    clear urls

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,9 @@ const Index = () => (
     <p>My Blog</p>
 
     <ul>
-      <PostLink title="Hello Next.js" />
-      <PostLink title="Learn Nest.js is awesome" />
-      <PostLink title="Deploy apps with Zeit" />
+      <PostLink id="hello-next-js" title="Hello Next.js" />
+      <PostLink id="learn-next-js-is-awesome" title="Learn Next.js is awesome" />
+      <PostLink id="deploy-apps-with-zeit" title="Deploy apps with Zeit" />
     </ul>
   </Layout>
 )
